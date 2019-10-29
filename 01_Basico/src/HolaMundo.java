1d748aa4fccd44c69779edeee84d5985dfd7c7dd
Profesor <profesor@cocim.aulas.es>
2019-10-29 14:52:23 +0100

    first commit

---
+++ 01_Basico/src/HolaMundo.java
@@ -0,0 +1,14 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		//Esto es un comentario de una linea
+		/*
+		 * Esto seria 
+		 * un comentario
+		 * de varias lineas
+		 */
+		System.out.println("Hola Mundo");
+		System.out.println("Otra vez hola mundo");
+	}
+}
