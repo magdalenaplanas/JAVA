0464370e60c52740c580ad9901e55f7674ed753f
Profesor <profesor@cocim.aulas.es>
2019-10-30 13:08:17 +0100

    ejercicio2

---
+++ 01_Ejercicios/src/Ejercicio2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		int numero = 0;
+		int numeroMayor = 0;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("introduzca un numero");
+			numero = sc.nextInt();
+			if (numero > numeroMayor) {
+				numeroMayor = numero;
+			}
+		} while (numero > 0);
+		System.out.println("El numero mayor es: " + numeroMayor);
+		System.out.println("Gracias por utilizar nuestro programa");
+		System.out.println("(tenemos microtransaccion :) :) :) )");
+	}
+
+}
