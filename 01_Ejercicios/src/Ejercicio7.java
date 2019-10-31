d37a1687290b055a940fb4be94f2076a2d82dc75
Profesor <profesor@cocim.aulas.es>
2019-10-31 14:44:58 +0100

    add strings y arrays

---
+++ 01_Ejercicios/src/Ejercicio7.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+		ArrayList<String> listaNombres = new ArrayList<String>();
+		Scanner sc = new Scanner(System.in);
+		String nombre = "";
+		
+		do {
+			System.out.println("Introduzca un nombre: (hasta FIN)");
+			nombre = sc.nextLine();
+			if(!nombre.equalsIgnoreCase("FIN")) {
+				listaNombres.add(nombre);
+			}			
+		}while(!nombre.equalsIgnoreCase("FIN"));
+
+		for(String valor : listaNombres) {
+			System.out.println(valor);
+		}
+	}
+
+}
