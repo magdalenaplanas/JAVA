636baeaaae33ebb80188775c02268c1c6592b2a3
Profesor <profesor@cocim.aulas.es>
2019-11-07 09:32:18 +0100

    ejercicio coche con constructor

--- Ejercicio01_Coche/src/Coche.java
+++ Ejercicio01_Coche/src/Coche.java
@@ -4,4 +4,30 @@ public class Coche {
 	public String marca;
 	public String modelo;
 	public boolean esNuevo;
+	
+	public Coche(String matricula, String marca, String modelo, boolean esNuevo) {
+		super();
+		this.matricula = matricula;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.esNuevo = esNuevo;
+	}
+
+	public Coche() {
+		super();
+		this.marca = "";
+	}
+
+	public Coche(String matricula) {
+		this();//esto llama al constructor sin parametros
+		this.matricula = matricula;
+	}
+
+	public Coche(String matricula, String marca) {
+		this();
+		this.matricula = matricula;
+		this.marca = marca;
+	}
+	
+	
 }
--- Ejercicio01_Coche/src/MainCoche.java
+++ Ejercicio01_Coche/src/MainCoche.java
@@ -10,7 +10,7 @@ public class MainCoche {
 		
 		Coche coche2 = new Coche();
 		coche2.matricula = "CFG1235";
-		coche2.marca = "Renault";
+		//coche2.marca = "Renault";
 		coche2.modelo = "Megane";
 		coche2.esNuevo = false;
 		
@@ -42,13 +42,26 @@ public class MainCoche {
 		imprimirCoche(coche2);
 		//imprimirCoche(coche2);
 		//imprimirCoche(coche2);
+		
+		System.out.println("*********** Ahora con constructores ************");
+		System.out.println();
+		Coche coche4 = new Coche("CWX1234");
+		coche4.marca = "Ferrari";
+		coche4.modelo = "Testarrosa";
+		imprimirCoche(coche4);
+		Coche coche5 = new Coche("TDE5678", "Wolkswagen");
+		imprimirCoche(coche5);
+		Coche coche6 = new Coche("HRT9876", "Audi", "A4", true);
+		imprimirCoche(coche6);
+		
 	}
 	
-	public static void imprimirCoche(Coche manolo) {
-		System.out.println("Matricula: " + manolo.matricula);
-		System.out.println("marca " + manolo.marca);
-		System.out.println("Modelo " + manolo.modelo);
-		System.out.println("Es nuevo? : " + manolo.esNuevo);
+	public static void imprimirCoche(Coche coche) {
+		System.out.println("Matricula: " + coche.matricula);
+		System.out.println("marca " + coche.marca);
+		System.out.println("Modelo " + coche.modelo);
+		System.out.println("Es nuevo? : " + coche.esNuevo);
+		System.out.println();
 	}
 	
 
